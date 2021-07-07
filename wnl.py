b2161d0255484c9950a4cc0b769f8b73a2863f88
Montana Mendy <montana@getprowl.com>
2021-07-07 02:32:21 -0700

    push wnl via vim

---
+++ wnl.py
@@ -0,0 +1,21 @@
+from random import seed
+
+from random import random
+print ("Welcome to the Wife N Life Calculator 2021\n" );
+
+
+job = input("What's your job?:")
+shoes = input("What kind of shoes do you wear?:")
+hobbies = input("What do you do for fun?:")
+wife = input("Do you want a wife by 2025?:")
+optional_information = input("Do you use MongoDB? CongoDB? Wear Cargo Shorts?:")
+wnl = input ("Do you think a wife n life % > 30 is possible for Montana?:")
+chances = input("Press enter to find out your Wife N Life %, given all 6 variables")
+
+seed(1)
+
+print(random(), random(), random())
+
+seed(1)
+
+print(random(), random(), random())
