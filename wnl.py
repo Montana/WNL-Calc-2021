b8e23a1ab3dcb5b686b2b4e197efb61ebca5c77d
Montana Mendy <montana@getprowl.com>
2021-07-07 11:31:52 -0700

    refactor -> travis.yml add

--- wnl.py
+++ wnl.py
@@ -3,14 +3,14 @@ from random import random
 
 print ("Welcome to the Wife N Life Calculator 2021\n" );
 
-job = input("What's your job?:")
-shoes = input("What kind of shoes do you wear?:")
-hobbies = input("What do you do for fun?:")
-wife = input("Do you want a wife by 2025?:")
-optional_information = input("Do you use MongoDB? CongoDB? Do you wear Cargo Shorts?:")
+job = input ("What's your job?:")
+shoes = input ("What kind of shoes do you wear?:")
+hobbies = input ("What do you do for fun?:")
+wife = input ("Do you want a wife by 2025?:")
+optional_information = input ("Do you use MongoDB? CongoDB? Do you wear Cargo Shorts?:")
 wnl = input ("Do you think a wife n life % > 30 is possible for Montana?:")
 total_ward = input ("Do you know a Jeff Ward?":)
-chances = input("Press enter to find out your Wife N Life %, given all 7 variables:")
+chances = input ("Press enter to find out your Wife N Life %, given all 7 variables:")
 
 seed(1)
 
