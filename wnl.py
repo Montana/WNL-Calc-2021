23d98f729cac9b687857d60f5f07f89e640f7698
Montana Mendy <montana@getprowl.com>
2021-07-07 02:42:59 -0700

    add consistency within the seeds

--- wnl.py
+++ wnl.py
@@ -9,7 +9,7 @@ hobbies = input("What do you do for fun?:")
 wife = input("Do you want a wife by 2025?:")
 optional_information = input("Do you use MongoDB? CongoDB? Wear Cargo Shorts?:")
 wnl = input ("Do you think a wife n life % > 30 is possible for Montana?:")
-chances = input("Press enter to find out your Wife N Life %, given all 6 variables")
+chances = input("Press enter to find out your Wife N Life %, given all 6 variables:")
 
 seed(1)
 
